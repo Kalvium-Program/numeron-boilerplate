a0cea862f503b71fc9fcaa7138c8b3f5b4209432
unknown <Ahamed1846>
2022-12-09 18:39:31 +0530

    made changes in  js

--- game.js
+++ game.js
@@ -1,5 +1,64 @@
 // Iteration 2: Generate 2 random number and display it on the screen
+let number1=document.getElementById('number1')
+let number2=document.getElementById('number2')
+let greaterThan=document.getElementById('greater-than')
+let equalTo=document.getElementById('equal-to')
+let lesserThan=document.getElementById('lesser-than')
 
+function randomGenerator(){
+number1.innerHTML=Math.floor(Math.random() * 100) + 1;
+number2.innerHTML=Math.floor(Math.random() * 100) + 1;
+}
 // Iteration 3: Make the options button functional
+var score=0
+greaterThan.onclick=()=>{
+    if (Number(number1.innerHTML)>Number(number2.innerHTML)){
+        score+=1
+        randomGenerator()
+        resetTime(timerId)
+    }else{
+        window.location='gameover.html'
+    }
+}
+equalTo.onclick=()=>{
+    if (Number(number1.innerHTML)==Number(number2.innerHTML)){
+        score+=1
+        randomGenerator()
+        resetTime(timerId)
+    }else{
+        window.location='gameover.html'
+    }
+}
+lesserThan.onclick=()=>{
+    if (Number(number1.innerHTML)<Number(number2.innerHTML)){
+        score+=1
+        randomGenerator()
+        resetTime(timerId)
+    }else{
+        window.location='gameover.html'
+    }
+}
 
-// Iteration 4: Build a timer for the game
+var time = 5;
+var timer = document.getElementById("timer");
+var timerId;
+
+function startTimer() {
+  timer.innerHTML = time;
+  timerId = setInterval(() => {
+    time--;
+    if (time == 0) {
+        window.location='gameover.html'
+    }
+    timer.innerHTML = time;
+  }, 1000);
+  sessionStorage.setItem("score", score);
+}
+
+function resetTime() {
+    time = 5;
+  clearInterval(timerId);
+  startTimer(timerId);
+}
+
+startTimer();
--- score.js
+++ score.js
@@ -1,1 +1,7 @@
 // Iteration 5: Store the player score and display it on the game over screen
+let playAgain=document.getElementById('play-again-button')
+let scoreBoard=document.getElementById('score-board')
+playAgain.onclick=()=>{
+    window.location='game.html'
+}
+scoreBoard.innerHTML=sessionStorage.getItem("score");
